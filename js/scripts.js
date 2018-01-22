4a3279c23f3b8eeba719245719f0f8d45063fa60
Jahmanz Williams and tyler Kostelak <jahmanz@>
2018-01-22 14:13:10 -0800

    jquery adds list of flavors in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,50 @@
-// business logic
 
+ var iceCreamFlavors = ["Cherry Garcia","Vanilla", "Spumoni", "Americone Dream", "tonight's Dough"];
 
+var countIceCream = 0;
 
-
-//user interface logic
 $(document).ready(function() {
+  $("button#flavors").click(function(event) {
+    $("ul#flavorList").append("<li> I love " + iceCreamFlavors[countIceCream] + "!</li>");
+      countIceCream ++;
+        if (countIceCream === 5) {
+          countIceCream = 0;
+        };
+  event.preventDefault();
+
+
+
+
+
+
+  // var iceFlav = iceCreamFlavors.forEach(function(flavor) {  $("#flavorList").append(" I love " + " " + flavor + "!");
+
 });
+  });
+
+
+
+// // business logic
+// var iceCreamFlavors = ["cherry-garcia","vanilla", "spumoni", "americone-dream", "tonight's-dough"];
+//
+// var iceFlav = iceCreamFlavors.forEach(function(flavor) {
+//   $("ul#flavors").prepend(flavor);
+// });
+//
+// //user interface logic
+//
+// $(document).ready(function(event) {
+//   $("button#clickFlavors").click(iceFlav(flavor) {}
+//
+//
+//  event.preventDefault();
+//   });
+
+
+// $(document).ready(function() {
+//   $("#")
+//   iceCreamFlavors.forEach(function(flavor){
+//     $("#flavors").text("I love " + flavor);
+//   });
+//
+//   });
